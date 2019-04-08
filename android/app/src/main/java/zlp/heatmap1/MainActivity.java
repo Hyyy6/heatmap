b2d893561d366a1dcef27a67d69905f8343e96d6
Hyyy6 <roman.tata.b@gmail.com>
2019-04-09 00:47:56 +0700

    an attempt to debug Java MainActivity and platform specific method implementation

--- android/app/src/main/java/zlp/heatmap1/MainActivity.java
+++ android/app/src/main/java/zlp/heatmap1/MainActivity.java
@@ -3,6 +3,7 @@ package zlp.heatmap1;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
@@ -12,21 +13,26 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        String msg = "zalupa";
+        Log.d(null, msg+'1');
         super.onCreate(savedInstanceState);
+        Log.d(null, msg+'2');
         GeneratedPluginRegistrant.registerWith(this);
 
         new MethodChannel(getFlutterView(), "zlp.heatmap1/wifi").setMethodCallHandler(
                 new MethodChannel.MethodCallHandler() {
                     @Override
                     public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                        Log.d(null, msg+'3');
                         if (methodCall.equals("getCurWifiLevel")) {
                             try {
                                 result.success(getCurWifiLevel());
+                                Log.d(null, msg+" success");
                             } catch (Exception e) {
                                 result.error("FAILED", "Failed to get current WiFi level", e.toString());
                             }
                         } else {
+                            Log.d(null, msg+'4');
                             result.notImplemented();
                         }
                     }
@@ -35,6 +41,7 @@ public class MainActivity extends FlutterActivity {
     }
 
     int getCurWifiLevel() {
+        Log.d(null, "zalupa 5");
         WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         return wifiManager.getConnectionInfo().getRssi();
     }
